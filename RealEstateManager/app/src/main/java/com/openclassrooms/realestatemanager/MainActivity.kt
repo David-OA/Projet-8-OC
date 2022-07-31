6d2cc9d01b9e34ffc209dde647dce9111d43bce7
Oconte David <david.oconte@orange.fr>
2022-07-31 14:15:53 +0200

    I migrate to kotlin.

---
+++ RealEstateManager/app/src/main/java/com/openclassrooms/realestatemanager/MainActivity.kt
@@ -0,0 +1,69 @@
+package com.openclassrooms.realestatemanager
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import butterknife.BindView
+import butterknife.ButterKnife
+import com.wangjie.rapidfloatingactionbutton.contentimpl.labellist.RFACLabelItem
+import com.wangjie.rapidfloatingactionbutton.contentimpl.labellist.RapidFloatingActionContentLabelList
+import pub.devrel.easypermissions.EasyPermissions
+
+class MainActivity : AppCompatActivity(), RapidFloatingActionContentLabelList.OnRapidFloatingActionContentLabelListListener<RFACLabelItem<Int>>, EasyPermissions.PermissionCallbacks {
+
+    private var textViewMain: TextView? = null
+    private var textViewQuantity: TextView? = null
+
+    @BindView(R.id.main_activity_toolbar) lateinit var toolbar: Toolbar
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        ButterKnife.bind(this)
+
+        //textViewMain = findViewById(R.id.activity_main_activity_text_view_main)
+        //textViewQuantity = findViewById(R.id.activity_main_activity_text_view_quantity)
+        //configureTextViewMain()
+        //configureTextViewQuantity()
+
+        configureToolbar()
+    }
+
+    // CONFIGURE UI
+
+    // ------ Toolbar ------
+    private fun configureToolbar() {
+        setSupportActionBar(toolbar);
+    }
+
+
+    private fun configureTextViewMain() {
+        textViewMain!!.textSize = 15f
+        textViewMain!!.text = "Le premier bien immobilier enregistré vaut "
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun configureTextViewQuantity() {
+        val quantity = Utils.convertDollarToEuro(100)
+        textViewQuantity!!.textSize = 20f
+        textViewQuantity!!.text = Integer.toString(quantity)
+    }
+
+    override fun onRFACItemLabelClick(position: Int, item: RFACLabelItem<RFACLabelItem<Int>>?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onRFACItemIconClick(position: Int, item: RFACLabelItem<RFACLabelItem<Int>>?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>) {
+        TODO("Not yet implemented")
+    }
+}
