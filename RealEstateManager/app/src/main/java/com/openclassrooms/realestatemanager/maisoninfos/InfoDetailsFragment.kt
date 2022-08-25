13b57c63ab53d3ecb0dd5801fe98c34f92d5f534
Oconte David <david.oconte@orange.fr>
2022-08-25 15:38:47 +0200

    I add to get the click on the listHouse for show the good info.

--- RealEstateManager/app/src/main/java/com/openclassrooms/realestatemanager/maisoninfos/InfoDetailsFragment.kt
+++ RealEstateManager/app/src/main/java/com/openclassrooms/realestatemanager/maisoninfos/InfoDetailsFragment.kt
@@ -10,6 +10,7 @@ import androidx.fragment.app.activityViewModels
 import com.openclassrooms.realestatemanager.MaisonsViewModel
 import com.openclassrooms.realestatemanager.R
 import com.openclassrooms.realestatemanager.databinding.FragmentMaisonsInfosBinding
+import com.openclassrooms.realestatemanager.model.Maison
 import com.smarteist.autoimageslider.SliderView
 
 class InfoDetailsFragment : Fragment() {
@@ -31,39 +32,40 @@ class InfoDetailsFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         val binding = FragmentMaisonsInfosBinding.bind(view)
 
-
         sliderView = binding.detailsViewSliderPictures
 
-        maisonsViewModel.currentMaison.observe(this.viewLifecycleOwner) {
-            binding.detailsViewDescription.text = it.detailsViewDescription
-            binding.detailViewNearTitle.text = it.detailViewNearTitle
-            binding.detailsViewSurface.text = it.detailsViewSurface
-            binding.detailsViewRooms.text = it.detailsViewRooms
-            binding.detailsViewBath.text = it.detailsViewBath
-            binding.detailsViewBed.text = it.detailsViewBed
-            binding.detailViewPrice.text = it.detailViewPrice
-            binding.detailViewType.text = it.detailViewType
-            binding.detailViewNearTitle.text = it.detailViewNearTitle
-            
+        val args = this.arguments
+        if (args != null) {
+            val maison = args.getSerializable("test") as Maison
+
+            binding.detailsViewDescription.text = maison.detailsViewDescription
+            binding.detailViewNearTitle.text = maison.detailViewNearTitle
+            binding.detailsViewSurface.text = maison.detailsViewSurface
+            binding.detailsViewRooms.text = maison.detailsViewRooms
+            binding.detailsViewBath.text = maison.detailsViewBath
+            binding.detailsViewBed.text = maison.detailsViewBed
+            binding.detailViewPrice.text = maison.detailViewPrice
+            binding.detailViewType.text = maison.detailViewType
+            binding.detailViewNearTitle.text = maison.detailViewNearTitle
+            sliderViewForInfoDetailsHome(maison.detailsViewSliderPicture)
+        } else {
+            maisonsViewModel.currentMaison.observe(this.viewLifecycleOwner) {
+                binding.detailsViewDescription.text = it.detailsViewDescription
+                binding.detailViewNearTitle.text = it.detailViewNearTitle
+                binding.detailsViewSurface.text = it.detailsViewSurface
+                binding.detailsViewRooms.text = it.detailsViewRooms
+                binding.detailsViewBath.text = it.detailsViewBath
+                binding.detailsViewBed.text = it.detailsViewBed
+                binding.detailViewPrice.text = it.detailViewPrice
+                binding.detailViewType.text = it.detailViewType
+                sliderViewForInfoDetailsHome(it.detailsViewSliderPicture)
+
+            }
         }
 
-        sliderViewForInfoDetailsHome()
     }
 
-    private fun  sliderViewForInfoDetailsHome() {
-
-        imageUrl = arrayOf(
-            R.drawable.photo1,
-            R.drawable.photo2,
-            R.drawable.photo3,
-            R.drawable.photo4,
-            R.drawable.photo5,
-            R.drawable.photo6,
-            R.drawable.photo7,
-            R.drawable.photo8,
-            R.drawable.photo9,
-            R.drawable.photo10
-        )
+    private fun  sliderViewForInfoDetailsHome(imageUrl: Array<Int>) {
 
         sliderAdapter = SliderAdapter(imageUrl)
         sliderView.autoCycleDirection = SliderView.LAYOUT_DIRECTION_LTR
