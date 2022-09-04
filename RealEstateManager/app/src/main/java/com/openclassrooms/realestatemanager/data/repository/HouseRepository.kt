a0a5255513ae96432be40ed7f28be92a94fbf8e1
Oconte David <david.oconte@orange.fr>
2022-09-04 16:41:26 +0200

    I add repository for add house in with dao and get all house.

---
+++ RealEstateManager/app/src/main/java/com/openclassrooms/realestatemanager/data/repository/HouseRepository.kt
@@ -0,0 +1,33 @@
+package com.openclassrooms.realestatemanager.data.repository
+
+import androidx.annotation.WorkerThread
+import com.openclassrooms.realestatemanager.data.HouseDao
+import com.openclassrooms.realestatemanager.model.House
+import kotlinx.coroutines.flow.Flow
+import kotlin.Suppress
+
+class HouseRepository (
+        private val houseDao: HouseDao
+){
+
+        val allHouses: Flow<List<House>> = houseDao.getAllHouse()
+
+        @Suppress("RedundantSuspendModifier")
+        @WorkerThread
+        suspend fun insert(house: House) {
+            houseDao.addHouse(house)
+        }
+
+        companion object {
+                @Volatile
+                private var INSTANCE: HouseRepository? = null
+                fun getHouseRepository(houseDao: HouseDao): HouseRepository {
+                        return INSTANCE
+                                ?: synchronized(this) {
+                                        val instance = HouseRepository(houseDao)
+                                        INSTANCE = instance
+                                        return instance
+                                }
+                }
+        }
+}
