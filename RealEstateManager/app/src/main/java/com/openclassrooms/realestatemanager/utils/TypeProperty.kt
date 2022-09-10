d65ffc1487ab9ed9acbd5d29c97c8607a941e642
Oconte David <david.oconte@orange.fr>
2022-09-10 15:42:12 +0200

    I add a list of type property.

---
+++ RealEstateManager/app/src/main/java/com/openclassrooms/realestatemanager/utils/TypeProperty.kt
@@ -0,0 +1,10 @@
+package com.openclassrooms.realestatemanager.utils
+
+enum class TypeProperty(val typeName: String) {
+    FLAT("Flat"),
+    TOWNHOUSE("Townhouse"),
+    PENTHOUSE("Penthouse"),
+    HOUSE("House"),
+    DUPLEX("Duplex"),
+    PALACE("Palace")
+}
