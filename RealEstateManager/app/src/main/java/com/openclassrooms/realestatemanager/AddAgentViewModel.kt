a67edb230d45351cf83b02e1b98bc69902ed6485
Oconte David <david.oconte@orange.fr>
2022-09-11 16:52:47 +0200

    I add the agent view model.

---
+++ RealEstateManager/app/src/main/java/com/openclassrooms/realestatemanager/AddAgentViewModel.kt
@@ -0,0 +1,14 @@
+package com.openclassrooms.realestatemanager
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.openclassrooms.realestatemanager.data.repository.AgentRepository
+import com.openclassrooms.realestatemanager.model.Agent
+import kotlinx.coroutines.launch
+
+class AddAgentViewModel (private val agentRepository: AgentRepository) : ViewModel() {
+
+    fun insert(agent: Agent) = viewModelScope.launch {
+        agentRepository.insert(agent)
+    }
+}
