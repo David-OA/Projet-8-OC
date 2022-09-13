874e2b6c1c0f03f4a033d52782a518992719af81
Oconte David <david.oconte@orange.fr>
2022-09-13 16:52:49 +0200

    I add this for get the name of agent selected for add the house.

--- RealEstateManager/app/src/main/java/com/openclassrooms/realestatemanager/addagent/AddAgentViewModel.kt
+++ RealEstateManager/app/src/main/java/com/openclassrooms/realestatemanager/addagent/AddAgentViewModel.kt
@@ -1,14 +1,29 @@
 package com.openclassrooms.realestatemanager.addagent
 
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.*
 import com.openclassrooms.realestatemanager.data.repository.AgentRepository
 import com.openclassrooms.realestatemanager.model.Agent
 import kotlinx.coroutines.launch
 
 class AddAgentViewModel (private val agentRepository: AgentRepository) : ViewModel() {
 
+    val getAllAgent: LiveData<List<Agent>> = agentRepository.allAgent.asLiveData()
+
     fun insert(agent: Agent) = viewModelScope.launch {
         agentRepository.insert(agent)
     }
+
+
+    // For selected the agent add the house
+    val getAgentClick = MutableLiveData<String>()
+
+    fun getNameClicked(agent: Agent) {
+        val agentFirstName = agent.firstName
+        val agentLastName = agent.lastName
+
+        val nameClicked = "$agentFirstName $agentLastName"
+        getAgentClick.value = nameClicked
+    }
+
+
 }
