ada9822d047bcdf831331b3a1cab68ad1fdf68a1
Oconte David <david.oconte@orange.fr>
2022-09-16 16:45:48 +0200

    I change for use intent for get info to edit.

--- RealEstateManager/app/src/main/java/com/openclassrooms/realestatemanager/editproperty/EditPropertyActivity.kt
+++ RealEstateManager/app/src/main/java/com/openclassrooms/realestatemanager/editproperty/EditPropertyActivity.kt
@@ -1,22 +1,13 @@
 package com.openclassrooms.realestatemanager.editproperty
 
 import android.os.Bundle
-import android.widget.Toast
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import com.openclassrooms.realestatemanager.addproperty.AddHouseViewModel
 import com.openclassrooms.realestatemanager.databinding.ActivityAddPropertyBinding
-import com.openclassrooms.realestatemanager.injection.Injection
-import com.openclassrooms.realestatemanager.injection.ViewModelFactory
 import com.openclassrooms.realestatemanager.model.House
 
 class EditPropertyActivity: AppCompatActivity() {
 
     private lateinit var binding: ActivityAddPropertyBinding
-
-    private val addHouseViewModel: AddHouseViewModel by viewModels {
-        ViewModelFactory(Injection.providesHouseRepository(this), Injection.providesAgentRepository(this))
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,8 +16,16 @@ class EditPropertyActivity: AppCompatActivity() {
         setContentView(view)
 
 
-        val house = addHouseViewModel.getHouseClicked.value
-        house?.detailsViewDescription
+        val houseIdEdit = intent.extras?.getSerializable("PropertyInFragment") as House
+
+        // Description
+        binding.addPropertyViewDescription.setText(houseIdEdit.detailsViewDescription)
+
+        // Address
+        binding.addPropertyViewAddress.setText(houseIdEdit.detailsAddress)
+
+        //Neighbourhood
+        binding.addPropertyViewNeighbourhood.setText(houseIdEdit.detailViewNearTitle)
 
 
     }
