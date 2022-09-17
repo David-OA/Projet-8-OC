9fe95f33221a3356a30816be583f9ba8e9fb751b
Oconte David <david.oconte@orange.fr>
2022-09-17 16:55:29 +0200

    I add all for see each element for change after.

--- RealEstateManager/app/src/main/java/com/openclassrooms/realestatemanager/editproperty/EditPropertyActivity.kt
+++ RealEstateManager/app/src/main/java/com/openclassrooms/realestatemanager/editproperty/EditPropertyActivity.kt
@@ -16,7 +16,33 @@ class EditPropertyActivity: AppCompatActivity() {
         setContentView(view)
 
 
+        getDataPropertySelectedToEdit()
+
+    }
+
+    private fun getDataPropertySelectedToEdit() {
         val houseIdEdit = intent.extras?.getSerializable("PropertyInFragment") as House
+
+        // Property Id
+        val houseId = houseIdEdit.houseId
+
+        //Property type
+        binding.addPropertyViewDropdownType.setText(houseIdEdit.detailViewType)
+
+        // Price
+        binding.addPropertyViewPrice.setText(houseIdEdit.detailViewPrice)
+
+        // Surface
+        binding.addPropertyViewSurface.setText(houseIdEdit.detailsViewSurface)
+
+        // Room
+        binding.addPropertyViewRoom.setText(houseIdEdit.detailsViewRooms)
+
+        // Bath
+        binding.addPropertyViewBathroom.setText(houseIdEdit.detailsViewBath)
+
+        // Bed
+        binding.addPropertyViewBedroom.setText(houseIdEdit.detailsViewBed)
 
         // Description
         binding.addPropertyViewDescription.setText(houseIdEdit.detailsViewDescription)
@@ -27,6 +53,38 @@ class EditPropertyActivity: AppCompatActivity() {
         //Neighbourhood
         binding.addPropertyViewNeighbourhood.setText(houseIdEdit.detailViewNearTitle)
 
+        // Near by
+        if (houseIdEdit.amenitySubway) {
+            binding.addPropertyViewNearbyGridlayoutNearby.checkboxNearbySubway.isChecked
+        }
+        if (houseIdEdit.amenitySchool) {
+            binding.addPropertyViewNearbyGridlayoutNearby.checkboxNearbySchool.isChecked
+        }
+        if (houseIdEdit.amenityShop) {
+            binding.addPropertyViewNearbyGridlayoutNearby.checkboxNearbyShop.isChecked
+        }
+        if (houseIdEdit.amenityPark) {
+            binding.addPropertyViewNearbyGridlayoutNearby.checkboxNearbyPark.isChecked
+        }
+        if (houseIdEdit.amenityPlayground) {
+            binding.addPropertyViewNearbyGridlayoutNearby.checkboxNearbyPlayground.isChecked
+        }
+        if (houseIdEdit.amenityBuses) {
+            binding.addPropertyViewNearbyGridlayoutNearby.checkboxNearbyBuses.isChecked
+        }
 
+        // On market since
+        binding.addPropertyViewSince.setText(houseIdEdit.detailMarketSince)
+
+        // Sold switch
+        if (houseIdEdit.detailSold) {
+            binding.addPropertyViewSoldSwitch.isChecked
+        }
+
+        // Sold on
+        binding.addPropertyViewSoldOn.setText(houseIdEdit.detailSoldOn)
+
+        // Agent add property
+        binding.addPropertyViewDropdownAgent.setText(houseIdEdit.detailManageBy)
     }
 }
