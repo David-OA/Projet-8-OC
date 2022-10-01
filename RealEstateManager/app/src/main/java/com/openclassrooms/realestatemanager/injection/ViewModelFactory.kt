5f86872d0b7dba15ac68ed4842c8aa00c1272477
Oconte David <david.oconte@orange.fr>
2022-10-01 14:18:21 +0200

    I add HouseViewModel.

--- RealEstateManager/app/src/main/java/com/openclassrooms/realestatemanager/injection/ViewModelFactory.kt
+++ RealEstateManager/app/src/main/java/com/openclassrooms/realestatemanager/injection/ViewModelFactory.kt
@@ -3,7 +3,6 @@ package com.openclassrooms.realestatemanager.injection
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.openclassrooms.realestatemanager.addagent.AddAgentViewModel
-import com.openclassrooms.realestatemanager.HouseViewModel
 import com.openclassrooms.realestatemanager.addproperty.AddHouseViewModel
 import com.openclassrooms.realestatemanager.data.repository.AgentRepository
 import com.openclassrooms.realestatemanager.data.repository.HouseRepository
@@ -15,7 +14,6 @@ class ViewModelFactory(
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         return when {
             modelClass.isAssignableFrom(AddHouseViewModel::class.java) -> AddHouseViewModel(houseRepository) as T
-            modelClass.isAssignableFrom(HouseViewModel::class.java) -> HouseViewModel() as T
             modelClass.isAssignableFrom(AddAgentViewModel::class.java) -> AddAgentViewModel(agentRepository) as T
             else -> throw Exception("Unknown ViewModel class")
         }
