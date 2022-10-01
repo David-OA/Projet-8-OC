e996fdb98ad0b626378f365cbad544c52f6c5842
Oconte David <david.oconte@orange.fr>
2022-10-01 14:13:43 +0200

    I clean the code.

--- RealEstateManager/app/src/main/java/com/openclassrooms/realestatemanager/utils/Currency.kt
+++ RealEstateManager/app/src/main/java/com/openclassrooms/realestatemanager/utils/Currency.kt
@@ -1,15 +1,6 @@
 package com.openclassrooms.realestatemanager.utils
 
-import java.text.NumberFormat
-import java.util.*
-
-
-    fun Double.toEuroDisplay(): String {
-        return NumberFormat.getNumberInstance(Locale.FRANCE).format(this)
-    }
-
-    var idGenerated: String = ""
-    get() {
-        field = UUID.randomUUID().toString()
-        return field
-    }
+enum class Currency(val  nameCurrency: String) {
+    EURO("EURO"),
+    DOLLAR("DOLLAR")
+}
