8d5bbeed11ac17c001fd528833a79c6820275fd0
Oconte David <david.oconte@orange.fr>
2022-10-03 16:34:01 +0200

    I add Adapter for the read and show picture save in internal storage.

--- RealEstateManager/app/src/main/java/com/openclassrooms/realestatemanager/houseinfos/PictureAdapter.kt
+++ RealEstateManager/app/src/main/java/com/openclassrooms/realestatemanager/houseinfos/PictureAdapter.kt
@@ -1,31 +1,84 @@
 package com.openclassrooms.realestatemanager.houseinfos
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.openclassrooms.realestatemanager.R
-import com.squareup.picasso.Picasso
+import com.openclassrooms.realestatemanager.addproperty.InternalStoragePhoto
+import com.openclassrooms.realestatemanager.databinding.PictureRecyclerviewItemBinding
 
-class PictureAdapter(imageUrl: Array<Int>) : RecyclerView.Adapter<PictureViewHolder>() {
+class PictureAdapter(
+    val pictureList: (InternalStoragePhoto) -> Unit
+) : ListAdapter<InternalStoragePhoto, PictureAdapter.PictureViewHolder>(Companion) {
 
-    var pictureList: Array<Int> = imageUrl
+    inner class PictureViewHolder(val binding: PictureRecyclerviewItemBinding): RecyclerView.ViewHolder(binding.root)
+
+    companion object : DiffUtil.ItemCallback<InternalStoragePhoto>() {
+        override fun areItemsTheSame(oldItem: InternalStoragePhoto, newItem: InternalStoragePhoto): Boolean {
+            return oldItem.bmp == newItem.bmp
+        }
+
+        @SuppressLint("DiffUtilEquals")
+        override fun areContentsTheSame(oldItem: InternalStoragePhoto, newItem: InternalStoragePhoto): Boolean {
+            return oldItem.name == newItem.name && oldItem.bmp.sameAs(newItem.bmp)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PictureViewHolder {
+        return PictureViewHolder(
+            PictureRecyclerviewItemBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: PictureViewHolder, position: Int) {
+        val photo = currentList[position]
+        holder.binding.apply {
+            myimage.setImageBitmap(photo.bmp)
+
+            /*val aspectRatio = photo.bmp.width.toFloat() / photo.bmp.height.toFloat()
+            ConstraintSet().apply {
+                clone(root)
+                setDimensionRatio(picturesAddedRvPicture.id, aspectRatio.toString())
+                applyTo(root)
+            }*/
+
+        }
+    }
+}
+
+
+/*(val pictureList: List<InternalStoragePhoto>) : RecyclerView.Adapter<PictureAdapter.PicturesViewHolder>() {
+
+    /*override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PicturesViewHolder {
+        val inflate: View = LayoutInflater.from(parent.context).inflate(R.layout.picture_recyclerview_item, parent,false)
+        return PicturesViewHolder(inflate)
+    }
 
     override fun getItemCount(): Int {
         return pictureList.size
     }
 
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PictureViewHolder {
-        val inflate: View = LayoutInflater.from(parent.context).inflate(R.layout.picture_recyclerview_item, null)
-        return PictureViewHolder(inflate)
-    }
-
-    override fun onBindViewHolder(holder: PictureViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: PicturesViewHolder, position: Int) {
             // if view holder is not null we are simply
-            // loading the image inside our image view using glide library
-            Picasso.get()
-                .load(pictureList[position])
-                .into(holder.imageView)
+
+        holder.bind(pictureList[position])
+
     }
-}
+
+    class PicturesViewHolder(val itemViewPicture: View) : RecyclerView.ViewHolder(itemViewPicture) {
+
+        fun bind(internalStoragePhoto: InternalStoragePhoto) = with(itemView) {
+
+            itemView.myimage.setImageBitmap(internalStoragePhoto.bmp)
+
+        }
+
+
+    }
+}*/
